181c118155b9f7659d44eb80bf6f9768d0ff5503
jamesenoviso <159095733+jamesenoviso@users.noreply.github.com>
2025-02-17 17:17:01 +0800

    create_students_table.sql

--- university_db/create_students_table.sql
+++ university_db/create_students_table.sql
@@ -5,6 +5,5 @@ CREATE TABLE `Students` (
     `StudentID` INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `FirstName` VARCHAR(50) NOT NULL,
     `LastName` VARCHAR(50) NOT NULL,
-    `EnrollmentDate` DATE NULL,
-    `Email` VARCHAR(100) NULL
+    `EnrollmentDate` DATE NULL
 );
