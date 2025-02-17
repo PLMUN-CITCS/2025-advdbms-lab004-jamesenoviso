8cb6f751fc694b916d6c88e2450c0cca7331a3bc
jamesenoviso <159095733+jamesenoviso@users.noreply.github.com>
2025-02-17 17:13:59 +0800

    create_students_table.sql

--- university_db/create_students_table.sql
+++ university_db/create_students_table.sql
@@ -2,9 +2,9 @@ Use `UniversityDB`;
 
 -- Step 1: Create the Students table with constraints
 CREATE TABLE `Students` (
-    StudentID INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    FirstName VARCHAR(50) NOT NULL,
-    LastName VARCHAR(50) NOT NULL,
-    EnrollmentDate DATE NULL,
-    Email VARCHAR(100) NULL
+    `StudentID` INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `FirstName` VARCHAR(50) NOT NULL,
+    `LastName` VARCHAR(50) NOT NULL,
+    `EnrollmentDate` DATE NULL,
+    `Email` VARCHAR(100) NULL
 );
